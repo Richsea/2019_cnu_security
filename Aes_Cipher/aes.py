128ffc67d9f74c4facdf0f2d9468ca4431fd05f5
Richsea <201402377@cs-cnu.org>
2019-09-29 18:01:49 +0900

    Finish aes homework

--- Aes_Cipher/aes.py
+++ Aes_Cipher/aes.py
@@ -4,6 +4,7 @@ import base64
 BS = 16
 pad = lambda s: s + (BS - len(s) % BS) * chr(BS - len(s) % BS)  
 unpad = lambda s : s[:-ord(s[len(s)-1:])]
+
 def getMode():
 	while True:
 		print('Enter either "encrypt" or "e" or "decrypt" or "d".')
@@ -17,55 +18,51 @@ def getFileName():
 	print('Enter your file name:')
 	return input()
 
-
 def crypto(mode, fileName):
-    keyValue = ''
-    iv = 'asefasdfefsdsdfe'
-    key = 'thisisbadkeyokey'
-    outputFileName = 'encrypt.txt'
+	keyValue = ''
+	iv = 'asefasdfefsdsdfe'
+	key = 'thisisbadkeyokey'
+	outputFileName = ''
+	if mode[0] == 'e':
+		outputFileName = 'encrypt.txt'
+	elif mode [0] == 'd':
+		outputFileName = 'decrypt.txt'
+	
+	translated = ''
+	outputFile = open(outputFileName, 'w')
+	
+	inputFile = open(fileName, 'r')
+	message = inputFile.read()
 
-    if mode[0] == 'd':
-        outputFileName = 'decrypt.txt'
-
-    translated = ''
-    outputFileName = open(outputFileName, 'w')
-    
-    inputFile = open(fileName, 'r')
-    message = inputFile.read()
-
-    if mode[0] == 'e':
-
-        message = pad(message)
-
-        cipher = AES.new(key, AES.MODE_CBC, iv)
-
-        console.log("after AES: " + message)
-        message = cipher.encrypt(message)
-
-        console.log("after encrypt: " + message)
-        message = base64.b64encode(message)
-
-        console.log("after base64: " + message)
-        translated = message
-
-    elif mode[0] == 'd':
-        message = message.decode("UTF-8")
-        message = base64.b64decode(message)
-
-        cipher = AES.new(key, AES.MODE_CBC, iv)
-        message = cipher.decrypt(message)
-        message = base64.b64decode(message)
-
-        message = unpad(message)
-        translated = message
-
-    outputFile.write(str(translated))
-    outputFile.close()
-    inputFile.close()
-    print('En(De)cryption complete')
-
-
-
+	iv = iv.encode("UTF-8")
+	key = key.encode("UTF-8")
+	
+	if mode[0] == 'e':
+		message = pad(message)
+		
+		cipher = AES.new(key, AES.MODE_CBC, iv)
+		
+		message = cipher.encrypt(message.encode('UTF-8'))
+		message = base64.b64encode(message)
+		
+		message = message.decode('UTF-8')
+		translated = message
+		
+	elif mode[0] == 'd':
+		message = message.encode('UTF-8')
+		message = base64.b64decode(message)
+		
+		cipher = AES.new(key, AES.MODE_CBC, iv)
+		message = cipher.decrypt(message)
+				
+		message = unpad(message)
+		message = message.decode('UTF-8')
+		translated = message
+	
+	outputFile.write(str(translated))
+	outputFile.close()
+	inputFile.close()
+	print('En(De)cryption complete')
 
 mode = getMode()
 fileName = getFileName()
